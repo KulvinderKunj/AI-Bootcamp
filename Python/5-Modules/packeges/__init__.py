06342e1cd6d4f27a4c56811edc07b94f3036ef5d
Kulvinder Singh <kalvinsingh777@gmail.com>
2025-10-01 00:07:17 +0100

    learn about built-in modules

---
+++ Python/5-Modules/packeges/__init__.py
@@ -0,0 +1,4 @@
+''' __init__.py is a special file used in pyhon packeges and 
+initialize their names '''
+
+# to tell the python treat this folder as package 
---
+++ Python/5-Modules/packeges/maths.py
@@ -0,0 +1,7 @@
+def addition(a,b):
+    return a+b
+
+
+
+def subb(a,b):
+    return a-b
